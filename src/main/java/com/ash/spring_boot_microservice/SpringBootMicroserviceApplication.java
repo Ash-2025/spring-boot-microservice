209792e69b3549c3d5188faf3bcae3dc6f1bf3dc
Ash-2025 <5880yash@gmail.com>
2025-10-10 07:28:59 +0530

    project init

---
+++ src/main/java/com/ash/spring_boot_microservice/SpringBootMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ash.spring_boot_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceApplication.class, args);
+	}
+
+}
